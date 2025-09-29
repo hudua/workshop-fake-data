7a9235eaadebac5982615475b1adb6970e375438
hudua <40040433+hudua@users.noreply.github.com>
2025-09-28 22:43:00 -0400

    Create external table for export of application data

---
+++ synapse/save_to_exp_layer.sql
@@ -0,0 +1,17 @@
+
+drop external table dbo.export
+CREATE EXTERNAL TABLE dbo.export
+WITH (
+    LOCATION = '/output-folder/ml-experimentation',
+    DATA_SOURCE = Experimentation,
+    FILE_FORMAT = CsvFormat
+)
+AS
+
+select x.SIN, approved, FLOOR(DATEDIFF(DAY, y.DateOfBirth, GETDATE()) / 365.25) AS AGE, avg(ApplicationCost) avgcost from esdc.dbo.applications x 
+join esdc.dbo.people y 
+    on x.SIN = y.SIN 
+join esdc.dbo.social_programs z 
+    on x.programID = z.programID
+
+group by x.SIN, approved, y.DateOfBirth
